95415f8087980e2f0d94a33f1603c60b2ddcfa60
Arpan Abhishek <arpanforbusiness@gmail.com>
2020-07-26 03:45:35 +0530

    make app installable

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ const header = document.querySelector("header");
 
 document.addEventListener("scroll", (e) => {
   var bodyRect = document.body.getBoundingClientRect();
-  if (bodyRect.y < -100) {
+  if (bodyRect.y < 0) {
     header.style.background = "white";
   } else {
     header.style.background = "transparent";
