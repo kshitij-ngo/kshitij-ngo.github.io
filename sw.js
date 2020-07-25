497e351318c1f9215039d42e5d544ea4aa21157e
Arpan Abhishek <arpanforbusiness@gmail.com>
2020-07-26 05:21:40 +0530

    fix sw.js

--- sw.js
+++ sw.js
@@ -10,11 +10,14 @@ const staticFiles = [
 // Cache all the files to make a PWA
 self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      // Our application only has two files here index.html and manifest.json
-      // but you can add more such as style.css as your app grows
-      return cache.addAll(staticFiles);
-    })
+    caches
+      .open(cacheName)
+      .then((cache) => {
+        // Our application only has two files here index.html and manifest.json
+        // but you can add more such as style.css as your app grows
+        return cache.addAll(staticFiles);
+      })
+      .then(self.skipWaiting())
   );
 });
 
