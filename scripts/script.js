f648520e06ae93fc970530c6a44210be6a8d5666
Arpan Abhishek <arpanforbusiness@gmail.com>
2020-07-27 03:25:40 +0530

    add install msg dialogue

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,17 @@
 const header = document.querySelector("header");
+const installMsg = document.querySelector("#install-msg");
+const installOkayBtn = document.querySelector("#install-msg .btn-div");
+const isMsgShown = localStorage.getItem("isMsgShown") || false;
+
+function disableInstallMsg() {
+  localStorage.setItem("isMsgShown", true);
+  installMsg.style.display = "none";
+}
+if (isMsgShown) {
+  disableInstallMsg();
+}
+
+installOkayBtn.addEventListener("click", disableInstallMsg);
 
 document.addEventListener("scroll", (e) => {
   var bodyRect = document.body.getBoundingClientRect();
